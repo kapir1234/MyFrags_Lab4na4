e60c4ac3e7245e0af11a084f6087edea2247387c
kapir1234 <kcpr.knp@gmail.com>
2020-10-23 15:12:49 +0200

    Lab3

--- app/src/main/java/com/example/myfrags/MainActivity.java
+++ app/src/main/java/com/example/myfrags/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.myfrags;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity implements Fragment1.OnButtonClickListener {
 
     private FragmentManager fragmentManager;
     private Fragment fragment1, fragment2, fragment3, fragment4;
+
+    private int[] frames;
+    private boolean hiden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +41,106 @@ public class MainActivity extends AppCompatActivity {
         transaction.add(R.id.frame4, fragment4);
         transaction.addToBackStack(null);
         transaction.commit();
+
+        if (savedInstanceState == null) {
+            frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
+            hiden = false;
+
+            Fragment[] fragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            transaction = fragmentManager.beginTransaction();
+            for (int i = 0; i < 4; i++) {
+                transaction.add(frames[i], fragments[i]);
+            }
+            transaction.addToBackStack(null);
+            transaction.commit();
+        } else {
+            frames = savedInstanceState.getIntArray("FRAMES");
+            hiden = savedInstanceState.getBoolean("HIDEN");
+        }
+
+    }
+
+    private void newFragments() {
+        Fragment[] newFragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(), new Fragment4()};
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        for (int i = 0; i < 4; i++) {
+            transaction.replace(frames[i], newFragments[i]);
+            if (hiden && !(newFragments[i] instanceof Fragment1)) transaction.hide(newFragments[i]);
+        }
+
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putIntArray("FRAMES", frames);
+        outState.putBoolean("HIDEN", hiden);
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment) {
+        super.onAttachFragment(fragment);
+
+        if (fragment instanceof Fragment1) {
+            ((Fragment1) fragment).setOnButtonClickListener(this);
+        }
+    }
+
+    //Listenery przycisków
+
+    @Override
+    public void onButtonClickShuffle() {
+        List<Integer> list = new ArrayList<Integer>(Arrays.asList(frames[0], frames[1], frames[2], frames[3]));
+        Collections.shuffle(list);
+        for (int i = 0; i < 4; i++) frames[i] = list.get(i).intValue();
+        newFragments();
+    }
+
+    @Override
+    public void onButtonClickClockwise() {
+
+        int t = frames[0];
+        frames[0] = frames[1];
+        frames[1] = frames[2];
+        frames[2] = frames[3];
+        frames[3] = t;
+
+        newFragments();
+    }
+
+    @Override
+    public void onButtonClickHide() {
+        if(hiden) return;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        for (Fragment f : fragmentManager.getFragments()) {
+            if (f instanceof Fragment1 ) continue;
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.hide(f);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        hiden = true;
+    }
+
+
+    @Override
+    public void onButtonClickRestore() {
+        if (!hiden) return;
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        for (Fragment f : fragmentManager.getFragments()) {
+            if (f instanceof Fragment1) continue;
+            transaction.show(f);
+        }
+        transaction.addToBackStack(null);
+        transaction.commit();
+        hiden = false;
     }
 }
